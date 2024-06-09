f99907c71024c4d07de1d25386e6b37e95aef460
balla-sravani <137860965+balla-sravani@users.noreply.github.com>
2024-06-09 13:11:58 +0530

    stringencodeing.py

--- DAY-2/stringencodeing.py
+++ DAY-2/stringencodeing.py
@@ -9,6 +9,7 @@ for i in s:
         d=d+chr(ord(i)-c+26)
 print(d)
 "khoor"
+" in a to z compare given String like 1st letter is k and give key is 3 so move 3 steps back from k so it is h next from h move 3 steps back it e like that it will continue "
 "3"
 "output:hello"
 
