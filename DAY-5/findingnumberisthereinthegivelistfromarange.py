e9c774b64792dc5893b85c85bb9967951a4ec315
balla-sravani <137860965+balla-sravani@users.noreply.github.com>
2024-06-12 09:12:14 +0530

    Add files via upload

---
+++ DAY-5/findingnumberisthereinthegivelistfromarange.py
@@ -0,0 +1,20 @@
+n=int(input())
+l=[0,5,3,6,7,2,1]
+'''
+for i in range(n+1):
+    if i in l:
+        continue
+    else:
+        k=i
+print(k)'''
+n=7
+b=sum(l)
+print(b)
+n=(n*(n+1))//2
+print(n)
+print(n-b)
+
+    
+
+    
+        
